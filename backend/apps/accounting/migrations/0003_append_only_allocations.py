d9f88be9853ad2ca1173ec056d7547402c0bb7e1
jeevan916 <matrixjeevan@gmail.com>
2026-09-10 11:11:09 +0530

    fix: correct allocation migration dependency import

--- backend/apps/accounting/migrations/0003_append_only_allocations.py
+++ backend/apps/accounting/migrations/0003_append_only_allocations.py
@@ -1,4 +1,5 @@
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -20,7 +21,7 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(
                 blank=True,
                 null=True,
-                on_delete=models.deletion.PROTECT,
+                on_delete=django.db.models.deletion.PROTECT,
                 related_name="reversal_entries",
                 to="accounting.paymentallocation",
             ),
