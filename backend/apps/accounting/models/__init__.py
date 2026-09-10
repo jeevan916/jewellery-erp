56b53cbc336e9bfc6a242bd44532f863eb6c9742
jeevan916 <matrixjeevan@gmail.com>
2026-09-10 21:22:43 +0530

    feat: expose sales accounting models

--- backend/apps/accounting/models/__init__.py
+++ backend/apps/accounting/models/__init__.py
@@ -1,17 +1,10 @@
 from .core import Account, AccountGroup, FinancialPeriod
 from .journal import JournalEntry, JournalEntryLine, Voucher, VoucherType
 from .billwise import BillReference, Party, Payment, PaymentAllocation
+from .sales import SalesInvoice, SalesInvoiceLine
 
 __all__ = [
-    "Account",
-    "AccountGroup",
-    "FinancialPeriod",
-    "Voucher",
-    "VoucherType",
-    "JournalEntry",
-    "JournalEntryLine",
-    "Party",
-    "BillReference",
-    "Payment",
-    "PaymentAllocation",
+    "Account", "AccountGroup", "FinancialPeriod", "Voucher", "VoucherType",
+    "JournalEntry", "JournalEntryLine", "Party", "BillReference", "Payment",
+    "PaymentAllocation", "SalesInvoice", "SalesInvoiceLine",
 ]
