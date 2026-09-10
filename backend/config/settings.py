ab7c5133fdd7c83b0865fb6c91d7da68ebc48fbd
jeevan916 <matrixjeevan@gmail.com>
2026-09-10 23:16:59 +0530

    fix: use built-in CORS middleware

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,7 +5,7 @@ SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY','development-only-change-me')
 DEBUG=os.environ.get('DJANGO_DEBUG','0')=='1'
 ALLOWED_HOSTS=[h.strip() for h in os.environ.get('DJANGO_ALLOWED_HOSTS','localhost,127.0.0.1').split(',') if h.strip()]
 INSTALLED_APPS=['django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','rest_framework','rest_framework.authtoken','apps.accounting.apps.AccountingConfig']
-MIDDLEWARE=['django.middleware.security.SecurityMiddleware','django.middleware.common.CommonMiddleware','django.middleware.cors.CorsMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware']
+MIDDLEWARE=['django.middleware.security.SecurityMiddleware','django.middleware.common.CommonMiddleware','config.cors.SimpleCORSMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware']
 ROOT_URLCONF='config.urls'
 TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]
 WSGI_APPLICATION='config.wsgi.application'; ASGI_APPLICATION='config.asgi.application'
@@ -13,4 +13,3 @@ DATABASES={'default':{'ENGINE':'django.db.backends.mysql','NAME':os.environ.get(
 LANGUAGE_CODE='en-us'; TIME_ZONE=os.environ.get('DJANGO_TIME_ZONE','Asia/Kolkata'); USE_I18N=True; USE_TZ=True
 STATIC_URL='static/'; DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 REST_FRAMEWORK={'DEFAULT_AUTHENTICATION_CLASSES':['rest_framework.authentication.TokenAuthentication'],'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.IsAuthenticated']}
-CORS_ALLOW_ALL_ORIGINS=True
